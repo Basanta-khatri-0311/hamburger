dc02ac29172cdf76cf2b5611683ece4a56547822
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-30 23:28:11 +0545

    event propagating stopping

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const hamburgerIcon = document.querySelector('.hamburger-menu-container')
-const headerContainer =  document.querySelector('.header-container')
+const headerContainer = document.querySelector('.header-container')
 const closeIcon = document.querySelector('.close-icon')
+const clickToTop = document.querySelector('.go-to-top')
 
-hamburgerIcon.addEventListener('click',()=>{
+hamburgerIcon.addEventListener('click', (e) => {
+  e.stopPropagation()
   headerContainer.classList.add('menu-open')
 })
-closeIcon.addEventListener('click',()=>{
+
+closeIcon.addEventListener('click', () => {
   headerContainer.classList.remove('menu-open')
+})
+window.addEventListener('click', () => {
+  headerContainer.classList.remove('menu-open')
+})
+
+
+clickToTop.addEventListener('click', () => {
+  window.scrollTo(0, 0)
 })
 
