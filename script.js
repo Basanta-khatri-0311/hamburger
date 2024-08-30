78aa34b0f624d2a76e608eacd2a8492f4fd383ad
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-30 23:48:19 +0545

    implementing scroll in goto top button

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const hamburgerIcon = document.querySelector('.hamburger-menu-container')
 const headerContainer = document.querySelector('.header-container')
 const closeIcon = document.querySelector('.close-icon')
-const clickToTop = document.querySelector('.go-to-top')
+const clickToTop = document.querySelector('.go-to-top a')
+const nav = document.querySelector('nav')
+const mainContent = document.querySelector('.main-content'); 
 
 hamburgerIcon.addEventListener('click', (e) => {
   e.stopPropagation()
   headerContainer.classList.add('menu-open')
 })
+
+nav.addEventListener('click',(e)=>e.stopPropagation())
 
 closeIcon.addEventListener('click', () => {
   headerContainer.classList.remove('menu-open')
@@ -17,6 +21,6 @@ window.addEventListener('click', () => {
 
 
 clickToTop.addEventListener('click', () => {
-  window.scrollTo(0, 0)
+  mainContent.scrollTo(0, 0)
 })
 
